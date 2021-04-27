93f918fb4c38772d441b5ac3ba4fe6a88448baab
ddot159 <dennisvu65@gmail.com>
2021-04-26 21:01:49 -0700

    fix: changed message to have non-unique condition.

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 class User(db.Model):
     # have the following columns
     # id (int)
@@ -23,7 +24,7 @@ class Messages(db.Model):
     # <Message: MESSAGE_GOES_HERE>
     # replace MESSAGE_GOES_HERE with the message
     
-    message = db.Column(db.String(256), unique=True, nullable=False)
+    message = db.Column(db.String(256), unique=False, nullable=False)
     id = db.Column(db.Integer, primary_key=True)
     user_id=db.Column(db.Integer, db.ForeignKey('user.id'))     
 
